b5e84548396411b7d77359de2a3ef33b97934240
Rafael Lunardelli <rafael@devsquad.com>
2023-07-18 15:04:34 -0600

    middleware :: mocking to make sure that the middleware is being called

--- tests/Feature/MiddlewaresTest.php
+++ tests/Feature/MiddlewaresTest.php
@@ -1,9 +1,11 @@
 <?php
 
 
+use App\Http\Middleware\SecureRouteMiddleware;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
+use function Pest\Laravel\mock;
 
 it('should block request if email is not jeremias@pinguim.academy', function () {
     $jeremias = User::factory()->create(['email' => 'jeremias@pinguim.academy']);
@@ -16,4 +18,12 @@ it('should block request if email is not jeremias@pinguim.academy', function () 
     actingAs($jeremias);
 
     get(route('secure-route'))->assertOk();
+});
+
+test('using mock', function () {
+    mock(SecureRouteMiddleware::class)
+        ->shouldReceive('handle')
+        ->atLeast()->once();
+
+    get(route('secure-route'));
 });
