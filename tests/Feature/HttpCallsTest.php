c727f0c1654bdd12d6b58ceaa625d1edc7c3577e
Rafael Lunardelli <rafael@devsquad.com>
2023-07-21 23:39:32 -0600

    making sure that we are sending the correct data to a fake api

--- tests/Feature/HttpCallsTest.php
+++ tests/Feature/HttpCallsTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Console\Commands\ExportProductToAmazon;
 use App\Console\Commands\ImportFromAmazonCommand;
+use App\Models\Product;
 use App\Models\User;
+use Illuminate\Http\Client\Request;
 use function Pest\Laravel\artisan;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
@@ -22,4 +25,28 @@ it('should fake an api request', function () {
     assertDatabaseHas('products', ['title' => 'Product 1']);
     assertDatabaseHas('products', ['title' => 'Product 2']);
     assertDatabaseCount('products', 2);
+});
+
+test('testing the data that we send to amazon', function () {
+    Http::fake();
+
+    config()->set('services.amazon.api_key', 123123131);
+
+    Product::factory()->count(2)->create();
+
+    (new ExportProductToAmazon)->handle();
+
+    Http::assertSent(function (Request $request) {
+        return $request->url() == 'https://api.amazon.com/products'
+            && $request->header('Authorization') == ['Bearer ' . config('services.amazon.api_key')]
+            && $request->data() == Product::all()->map(fn($p) => ['title' => $p->title])->toArray();
+    });
+
+});
+
+it('my config should have at least the key', function () {
+    expect(config('services'))
+        ->toHaveKey('amazon')
+        ->and(config('services.amazon'))
+        ->toHaveKey('api_key');
 });
