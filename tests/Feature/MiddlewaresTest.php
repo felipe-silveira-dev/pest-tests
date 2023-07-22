ec350a956ae1ad41414af5c1aa8cd63a30182bee
Rafael Lunardelli <rafael@devsquad.com>
2023-07-21 22:44:06 -0600

    middlewares :: making sure that the middleware is being used in a particularly route

--- tests/Feature/MiddlewaresTest.php
+++ tests/Feature/MiddlewaresTest.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middleware\JeremiasMiddleware;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
+use function Pest\Laravel\mock;
 
 it('should block a request if the user does not have the following email: jeremias@pinguim.academy', function () {
     $user = User::factory()->create(['email' => 'email@qualquer.com']);
@@ -13,4 +15,12 @@ it('should block a request if the user does not have the following email: jeremi
 
     actingAs($jeremias);
     get(route('secure-route'))->assertOk();
+});
+
+test('check if is being called', function () {
+    mock(JeremiasMiddleware::class)
+        ->shouldReceive('handle')
+        ->atLeast()->once();
+
+    get(route('secure-route'));
 });
