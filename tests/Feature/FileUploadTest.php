0704a0fff298aeff98972589d1f3c09c0363093b
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-04-09 13:40:59 -0300

    finish curso

--- tests/Feature/FileUploadTest.php
+++ tests/Feature/FileUploadTest.php
@@ -2,6 +2,8 @@
 
 use App\Models\User;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
